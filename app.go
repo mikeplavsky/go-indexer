a6d5b4b762ec513c2c5f24b9db8265693a79ac78
mikeplavsky <mike.plavsky@quest.com>
2015-02-11 18:18:33 +0000

    getting from s3

--- app.go
+++ app.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
+	"os/exec"
 
 	"github.com/goamz/goamz/aws"
+	"github.com/goamz/goamz/s3"
 	"github.com/goamz/goamz/sqs"
 )
 
@@ -18,10 +22,9 @@ func main() {
 		log.Println(err)
 	}
 
-	log.Println(q)
-
 	ps := map[string]string{
-		"WaitTimeSeconds": "10"}
+		"WaitTimeSeconds":     "10",
+		"MaxNumberOfMessages": "1"}
 
 	res, err := q.ReceiveMessageWithParameters(ps)
 
@@ -29,6 +32,53 @@ func main() {
 		log.Println(err)
 	}
 
-	log.Println(res)
+	raw := res.Messages[0].Body
+
+	var msg map[string]interface{}
+	err = json.Unmarshal([]byte(raw), &msg)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	bucket := msg["bucket"].(string)
+	path := msg["path"].(string)
+
+	log.Println(path)
+
+	s3 := s3.New(auth, aws.USEast)
+	b := s3.Bucket(bucket)
+
+	data, err := b.Get(path)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	f, _ := ioutil.TempFile(
+		"",
+		"indexer")
+
+	defer f.Close()
+
+	f.Write(data)
+	f.Sync()
+
+	log.Println(f.Name())
+
+	exec := func(cmd string) {
+
+		cvrt := exec.Command("bash", "-c", cmd)
+
+		_, err = cvrt.Output()
+		if err != nil {
+			log.Println()
+		}
+
+	}
+
+	exec("unzip -p  " + f.Name() + " | ./go-convert")
+
+	q.DeleteMessage(&res.Messages[0])
 
 }
