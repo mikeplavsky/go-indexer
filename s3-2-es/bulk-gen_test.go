ac44ee068870e2c8e4eb63ee9746db4e6a0316ec
Ubuntu <ubuntu@ip-10-67-166-168.ec2.internal>
2015-02-20 23:10:40 +0000

    started s3-2-es part

---
+++ s3-2-es/bulk-gen_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestParseTimestamp(t *testing.T) {
+
+	cases := []struct {
+		uri, expectedTimestamp string
+	}{
+		{"s3://s3bucket/path/mage_20231123_122134.zip", "2023-11-23T12:21:34Z"},
+		{"s3://s3bucket/path/mage.08.28.1998.zip", "1998-08-28T00:00:00Z"}, //legacy format: US digitd order, Europerian dots, akward
+		{"s3://dmp-log-analysis/Quadra2901/UKTCEUCMIGPRDV1/UnifiedCalendarSync/MAgE_20150129_031000 (1).zip", "2015-01-29T03:10:00Z"},
+		{"", ""},
+	}
+
+	for _, testCase := range cases {
+		timestamp, err := ParseTimestamp(testCase.uri)
+		assert.Equal(t, testCase.expectedTimestamp, timestamp, fmt.Sprintf("they should be equal. Error: %s", err))
+	}
+}
+
+func TestParseLine(t *testing.T) {
+	res, err := ParseLine("1010760	buck-et/C-UST-OM-ER/F O L D/ E R/Log file_20150112_135921.zip")
+	assert.Equal(t, nil, err, fmt.Sprintf("they should be equal. Error: %s", err))
+	assert.Equal(t, "1010760", res["size"])
+	assert.Equal(t, "C-UST-OM-ER", res["customer"])
+	assert.Equal(t, "2015-01-12T13:59:21Z", res["@timestamp"])
+	assert.Equal(t, "buck-et/C-UST-OM-ER/F O L D/ E R/Log file_20150112_135921.zip", res["uri"])
+}
