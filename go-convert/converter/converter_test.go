40e68de13b7cc4e665907a453419cb246068b34d
mikeplavsky <mike.plavsky@quest.com>
2015-02-21 11:39:38 +0000

    index test

--- go-convert/converter/converter_test.go
+++ go-convert/converter/converter_test.go
@@ -1,16 +1,47 @@
 package convert
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"os"
 	"strings"
 	"testing"
 )
 
-func TestMain(m *testing.M) { 
+func TestMain(m *testing.M) {
 
 	os.Remove("/tmp/mage.json")
-	os.Exit(m.Run()) 
+	os.Exit(m.Run())
+
+}
+
+func TestIndex(t *testing.T) {
+
+	var parse = func(
+		path,
+		line string,
+		num int) (string, error) {
+		return "", nil
+	}
+
+	r := strings.NewReader("one")
+
+	Convert("path", r, parse)
+
+	f, _ := ioutil.ReadFile("/tmp/mage.json")
+	res := strings.Split(string(f), "\n")
+
+	var idx map[string]map[string]string
+
+	err := json.Unmarshal([]byte(res[0]), &idx)
+
+	if err != nil {
+		t.Error(res[0], err)
+	}
+
+	if idx["index"]["_type"] != "log" {
+		t.Error(res[0], "wrong index type")
+	}
 
 }
 
