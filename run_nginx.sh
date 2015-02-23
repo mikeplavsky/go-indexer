4573ab98a0f3cb99f3b197c8de1777dff9cecf26
mikeplavsky <mike.plavsky@quest.com>
2015-02-23 15:07:53 +0000

    ui and es exposed through nginx

--- run_nginx.sh
+++ run_nginx.sh
@@ -1,5 +1,7 @@
 docker create  \
+-v $(pwd)/ui:/data/www \
 -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro \
+-p 443:443 \
 --name=nginx --net=host --restart=always mikeplavsky/docker-nginx
 
 docker start nginx
