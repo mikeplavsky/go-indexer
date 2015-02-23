3ecc9089e5be11f3b12c19787ba46087a1d813f9
mikeplavsky <mike.plavsky@quest.com>
2015-02-23 22:01:15 +0000

    getting query

--- rest-srv/app.go
+++ rest-srv/app.go
@@ -1,15 +1,25 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-martini/martini"
+)
+
+func getJob(r *http.Request) string {
+
+	log.Println(r.URL.Query())
+	return `{"count":12, "size": 220}`
+
+}
 
 func main() {
 
 	m := martini.Classic()
 	m.Use(martini.Logger())
 
-	m.Get("/job", func() string {
-		return `{"count":12, "size": 220}`
-	})
+	m.Get("/job", getJob)
 
 	m.Run()
 
