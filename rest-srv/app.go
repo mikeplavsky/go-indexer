c5ba00618ce5f8799a33c024c8df90a3c26a36ba
mikeplavsky <mike.plavsky@quest.com>
2015-02-23 23:00:49 +0000

    started es

--- rest-srv/app.go
+++ rest-srv/app.go
@@ -5,11 +5,18 @@ import (
 	"net/http"
 
 	"github.com/go-martini/martini"
+	es "github.com/mattbaird/elastigo/lib"
 )
 
 func getJob(r *http.Request) string {
 
 	log.Println(r.URL.Query())
+
+	c := es.NewConn()
+
+	c.Domain = "localhost"
+	c.Port = "8080"
+
 	return `{"count":12, "size": 220}`
 
 }
