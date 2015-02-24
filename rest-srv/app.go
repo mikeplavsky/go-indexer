dc061c51132b79bb44f7645d880b0fa6c3c38778
Maxim.Maximov <Maxim.Maximov@software.dell.com>
2015-02-24 12:13:05 +0000

    calculate

--- rest-srv/app.go
+++ rest-srv/app.go
@@ -3,22 +3,42 @@ package main
 import (
 	"log"
 	"net/http"
+	"fmt"
 
 	"github.com/go-martini/martini"
-	es "github.com/mattbaird/elastigo/lib"
+	"github.com/olivere/elastic"
 )
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func getJob(r *http.Request) string {
+	#todo:dehartdcode
+	url := "http://localhost:8080"
 
 	log.Println(r.URL.Query())
 
-	c := es.NewConn()
+	client, err := elastic.NewClient(http.DefaultClient, url)
+	check(err)
 
-	c.Domain = "localhost"
-	c.Port = "8080"
+	searchResult, err := client.Search().
+		Index("s3data").  // search in index "twitter"
+		From(0).Size(10). // take documents 0-9
+		Debug(true).      // print request and response to stdout
+		Pretty(true).     // pretty print request and response JSON
+		Do()              // execute
+	check(err)
 
-	return `{"count":12, "size": 220}`
+	if searchResult.Hits != nil {
+		#todo:use json.marchal
+		log.Printf("Found a total of %d tweets\n", searchResult.Hits.TotalHits)
+		return fmt.Sprintf(`{"count":%d, "size": 220}`,  searchResult.Hits.TotalHits)
+	}
 
+	panic("todo:return 500")
 }
 
 func main() {
