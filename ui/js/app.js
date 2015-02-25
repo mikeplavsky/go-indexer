37be012fb5fb341ef16db62f86e6de7b9d061d78
2xmax <2xmax@mail.ru>
2015-02-25 05:20:38 +0300

    reformatted js and css

--- ui/js/app.js
+++ ui/js/app.js
@@ -1,51 +1,48 @@
 angular.module('myApp', ['cui'])
-    .controller('AppController', function($scope,
-        cuiDataSourceService,
-        cuiAlertService,
-        cuiLoading) {
+    .controller('AppController', function ($scope,
+                                           cuiDataSourceService,
+                                           cuiAlertService,
+                                           cuiLoading) {
 
         $scope.applicationframe = {};
 
         $scope.job = {}
 
+        $scope.job.customers = []
 
-	$scope.job.customers = []
+        customerSvc = cuiDataSourceService('api/customers');
 
-	customerSvc = cuiDataSourceService('api/customers');
-	
-	customerSvc.query()
-		.then(function(res){
-			$scope.job.customers = res.result.map(function(value, key) {
-				       return { 
-  						label: value,
-  						description: value
-					};
-			});
-		});
+        customerSvc.query()
+            .then(function (res) {
+                $scope.job.customers = res.result.map(function (value, key) {
+                    return {
+                        label: value,
+                        description: value
+                    };
+                });
+            });
 
-        $scope.calculate = function() {
+        $scope.calculate = function () {
 
             srv = cuiDataSourceService('/api/job');
 
             cuiLoading(
-
                 srv.query($scope.job)
-                .then(function(res) {
+                    .then(function (res) {
 
                         $scope.job.count = res.count;
                         $scope.job.size = res.size;
 
                     },
-                    function(err) {
+                    function (err) {
                         cuiAlertService.warning(err);
                     })
-
             );
         }
     })
-    .controller('AboutBoxCtrl', function($scope, cuiAboutBox) {
-      var aboutBox = cuiAboutBox({
-          applicationName: 'Logs Indexer'
-      });
-      $scope.showAboutBox = aboutBox.modal.show;
-});
+    .controller('AboutBoxCtrl', function ($scope, cuiAboutBox) {
+        var aboutBox = cuiAboutBox({
+            applicationName: 'Logs Indexer'
+        });
+        $scope.showAboutBox = aboutBox.modal.show;
+    });
