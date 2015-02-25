b5e3cc5c49836b010262ad39c4a8dc882b7a2ee8
mikeplavsky <mike.plavsky@quest.com>
2015-02-25 07:46:31 +0000

    all images in one place for preparing amis

---
+++ prep_env.sh
@@ -0,0 +1,8 @@
+docker pull mikeplavsky/elasticsearch
+docker pull mikeplavsky/kibana
+docker pull mikeplavsky/docker-golang
+docker pull mikeplavsky/docker-nginx
+
+docker build -t go_indexer .
+
+
