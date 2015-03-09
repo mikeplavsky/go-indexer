c017c4bc68d2979a6b42228c73a0b61a396cbccc
mikeplavsky <mike.plavsky@quest.com>
2015-03-09 12:35:40 +0000

    counting lines, a lot of duplication with indexer

---
+++ go-counter/app.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"go-indexer/go-send/sender"
+	"log"
+	"strconv"
+
+	"github.com/dustin/go-humanize"
+)
+
+func getCount() (uint64, error) {
+
+	q, err := sender.GetQueue()
+
+	if err != nil {
+		return 0, err
+	}
+
+	ps := map[string]string{
+		"WaitTimeSeconds":     "10",
+		"MaxNumberOfMessages": "1"}
+
+	res, err := q.ReceiveMessageWithParameters(ps)
+
+	if err != nil {
+		return 0, err
+	}
+
+	if len(res.Messages) == 0 {
+		return 0, errors.New("No messages")
+	}
+
+	q.DeleteMessage(&res.Messages[0])
+
+	raw := res.Messages[0].Body
+
+	var msg map[string]interface{}
+	err = json.Unmarshal([]byte(raw), &msg)
+
+	if err != nil {
+		return 0, err
+	}
+
+	r := msg["path"].(string)
+	count, _ := strconv.Atoi(r)
+
+	return uint64(count) / 2, nil
+
+}
+
+func main() {
+
+	count := uint64(0)
+
+	for {
+		c, err := getCount()
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		count += c
+		log.Println(humanize.Comma(int64(count)))
+	}
+
+}
