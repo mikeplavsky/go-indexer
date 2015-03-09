c7bdd2305e84000951c4fc6253eb0c83b5744207
mikeplavsky <mike.plavsky@quest.com>
2015-03-09 20:52:48 +0000

    added _source and returned back indexing

--- indexer.sh
+++ indexer.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-#unzip -p $ES_FILE | go-convert > /tmp/mage.json
+#ES_QUEUE=$(curl http://169.254.169.254/latest/meta-data/instance-id)-count
+#unzip -p $ES_FILE | go-convert | wc -l | awk '{print "count/"$0}' | go-send s
 
-ES_QUEUE=$(curl http://169.254.169.254/latest/meta-data/instance-id)-count
-unzip -p $ES_FILE | wc -l | awk '{print "count/"$0}' | go-send s
-
-#curl -S -XPOST localhost:$ES_PORT/$ES_INDEX/_bulk --data-binary @/tmp/mage.json > /dev/null
+unzip -p $ES_FILE | go-convert > /tmp/mage.json
+curl -S -XPOST localhost:$ES_PORT/$ES_INDEX/_bulk --data-binary @/tmp/mage.json > /dev/null
