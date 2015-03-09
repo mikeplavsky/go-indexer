0f49c01e5c9db725aa2d9e12a64a52a80b7031eb
mikeplavsky <mike.plavsky@quest.com>
2015-03-09 09:54:04 +0000

    counting messages

--- indexer.sh
+++ indexer.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 unzip -p $ES_FILE | go-convert > /tmp/mage.json
-curl -S -XPOST localhost:$ES_PORT/$ES_INDEX/_bulk --data-binary @/tmp/mage.json > /dev/null
+
+ES_QUEUE=$(curl http://169.254.169.254/latest/meta-data/instance-id)-count
+cat /tmp/mage.json | wc -l | awk '{print "count/"$0}' | go-send s
+
+#curl -S -XPOST localhost:$ES_PORT/$ES_INDEX/_bulk --data-binary @/tmp/mage.json > /dev/null
