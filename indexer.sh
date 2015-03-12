7d608143cf4573f4a3607008fe5e186ef664c7cc
mikeplavsky <mike.plavsky@quest.com>
2015-03-12 07:22:31 +0000

    memory

--- indexer.sh
+++ indexer.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
+THROTTLING_LINES_PER_UPLOAD=100000
+rm -rf ./x*
+
 unzip -p $ES_FILE | go-convert > /tmp/mage.json
-curl -S -XPOST localhost:$ES_PORT/$ES_INDEX/_bulk --data-binary @/tmp/mage.json > /dev/null
+split -d -l $THROTTLING_LINES_PER_UPLOAD /tmp/mage.json
+
+echo "Posting data"
+
+for f in ./x*; 
+do
+	echo $f;
+	curl -S -XPOST localhost:$ES_PORT/$ES_INDEX/_bulk --data-binary @$f > /dev/null;
+done
+
