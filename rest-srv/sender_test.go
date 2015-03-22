d9b78893166fa9db97d72f23a2191a0257547110
mikeplavsky <mike.plavsky@quest.com>
2015-03-22 14:48:38 +0300

    paging tests

--- rest-srv/sender_test.go
+++ rest-srv/sender_test.go
@@ -57,7 +57,7 @@ func TestSendJobZero(t *testing.T) {
 	assert.NoError(t, err)
 }
 
-func TestSend(t *testing.T) {
+func TestSendQueues(t *testing.T) {
 
 	mNum := 9
 	qNum := 4
@@ -91,7 +91,7 @@ func TestSend(t *testing.T) {
 		m string
 	}
 
-	res := make(chan r, mNum*qNum)
+	res := make(chan r, mNum)
 
 	send := func(q int, m string) {
 		res <- r{q, m}
@@ -130,5 +130,92 @@ func TestSend(t *testing.T) {
 	assert.True(t, f[0]["4"])
 	assert.True(t, f[1]["1"])
 	assert.True(t, f[3]["7"])
+
+}
+
+func TestSendPaging(t *testing.T) {
+
+	total := 8
+	mNum := 4
+	qNum := 4
+
+	PageSize = 2
+
+	nextId := make(chan int)
+	go func() {
+		for i := 0; ; i++ {
+			nextId <- i
+		}
+	}()
+
+	getFiles = func(job job,
+		skip int,
+		take int) (*elastic.SearchHits, error) {
+
+		hits := elastic.SearchHits{
+			TotalHits: int64(total)}
+
+		for i := 0; i < mNum; i++ {
+
+			msg := fmt.Sprintf(
+				`{"uri":"https://s3.amazonaws.com/%v/%v/%v"}`,
+                                skip,
+                                take,
+				<-nextId)
+
+			raw := json.RawMessage(msg)
+
+			hits.Hits = append(
+				hits.Hits,
+				&elastic.SearchHit{
+					Source: &raw})
+
+		}
+
+		return &hits, nil
+	}
+
+	type r struct {
+		q int
+		m string
+	}
+
+	res := make(chan r, total/PageSize*mNum)
+
+	send := func(q int, m string) {
+		res <- r{q, m}
+	}
+
+	tq := tq{s: send, n: qNum}
+
+	err := sendJobImpl(job{}, tq)
+	close(res)
+
+	assert.NoError(t, err)
+	f := map[int]map[string]bool{}
+
+	for r := range res {
+
+		t.Log(r)
+
+		if _, ok := f[r.q]; !ok {
+			f[r.q] = map[string]bool{}
+		}
+
+		f[r.q][r.m] = true
+	}
+
+	for q := 0; q < qNum; q++ {
+
+		t.Log("Queue:", q)
+		t.Log(f[q])
+
+		assert.Equal(t, 4, len(f[q]))
+
+	}
+
+	assert.True(t, f[0]["2/2/4"])
+	assert.True(t, f[1]["0/2/1"])
+	assert.True(t, f[3]["2/2/7"])
 
 }
