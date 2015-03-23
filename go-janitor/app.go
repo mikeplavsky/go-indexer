4b27ffec76617682574093ebb94511bd61343096
mikeplavsky <mike.plavsky@quest.com>
2015-03-23 10:35:58 +0300

    started deleting queues

--- go-janitor/app.go
+++ go-janitor/app.go
@@ -3,12 +3,101 @@ package main
 import (
 	"fmt"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/goamz/goamz/aws"
 	"github.com/goamz/goamz/ec2"
 	"github.com/goamz/goamz/sqs"
+	"github.com/golang/glog"
+
+	"github.com/cenkalti/backoff"
+	"github.com/rubyist/circuitbreaker"
 )
+
+type T interface{}
+
+func retryCall(
+	f func() (T, error)) T {
+
+	for {
+
+		var res T
+
+		get := func() error {
+
+			return backoff.Retry(func() error {
+
+				r, err := f()
+				res = r
+
+				return err
+
+			}, backoff.NewExponentialBackOff())
+
+		}
+
+		cb := circuit.NewBreaker()
+		err := cb.Call(get, time.Second*120)
+
+		if err != nil {
+
+			glog.Info(err)
+			continue
+
+		}
+
+		return res
+
+	}
+
+}
+
+func deleteQueue(
+	qUrl string,
+	ec *ec2.EC2,
+	sqs *sqs.SQS) {
+
+	qn := strings.Split(qUrl, "/")[4]
+	id := qn[:10]
+
+	opts := ec2.DescribeInstanceStatusOptions{
+		InstanceIds: []string{id}}
+
+	_, err := ec.DescribeInstanceStatus(&opts,
+		ec2.NewFilter())
+
+	if err != nil {
+
+		fmt.Println(err)
+
+		s := err.Error()
+
+		if !strings.Contains(
+			s,
+			"InvalidInstanceID.NotFound") {
+			return
+		}
+
+		q, err := sqs.GetQueue(qn)
+
+		if err != nil {
+			return
+		}
+
+		_, err = q.Delete()
+
+		if err != nil {
+			return
+		}
+
+		fmt.Print(".")
+		return
+	}
+
+	return
+
+}
 
 func main() {
 
@@ -19,22 +108,15 @@ func main() {
 
 	ec := ec2.New(auth, aws.USEast)
 
+	var w sync.WaitGroup
+
 	for _, i := range res.QueueUrl {
 
-		id := strings.Split(i, "/")[4]
-
-		opts := ec2.DescribeInstanceStatusOptions{
-			InstanceIds: []string{id}}
-
-		res, err := ec.DescribeInstanceStatus(&opts,
-			ec2.NewFilter())
-
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		fmt.Println(res)
+		w.Add(1)
+		go deleteQueue(i, ec, sqs)
 
 	}
+
+	w.Wait()
 
 }
