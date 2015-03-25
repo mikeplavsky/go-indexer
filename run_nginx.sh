1f1b11f1462cc4aa27665da2cb1cb71cf94b7705
mikeplavsky <mike.plavsky@quest.com>
2015-03-25 11:28:17 +0000

    secured nginx

--- run_nginx.sh
+++ run_nginx.sh
@@ -1,6 +1,7 @@
 docker create  \
 -v $(pwd)/ui:/data/www \
 -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro \
+-v $(pwd)/.htpasswd:/etc/nginx/.htpasswd:ro \
 -p 443:443 \
 --name=nginx --net=host --restart=always mikeplavsky/docker-nginx
 
