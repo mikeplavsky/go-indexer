d44e1e0bca24db797b3f3120a94e009d7737165d
mikeplavsky <mike.plavsky@quest.com>
2015-03-30 14:17:17 +0000

    no tty when starting

--- create_queues.sh
+++ create_queues.sh
@@ -2,6 +2,6 @@ ES_QUEUE=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 
 docker rm -f queues
 
-docker run -ti \
+docker run \
 -e ES_QUEUE=$ES_QUEUE \
 --name=queues go_indexer go-send c
