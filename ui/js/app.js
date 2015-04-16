9790d015535e0ed505f340cb3d333e77917cff89
mikeplavsky <mike.plavsky@quest.com>
2015-04-17 00:45:45 +0300

    incremented version

--- ui/js/app.js
+++ ui/js/app.js
@@ -142,7 +142,7 @@ angular.module('myApp', ['cui'])
     })
     .controller('AboutBoxCtrl', function ($scope, cuiAboutBox) {
         var aboutBox = cuiAboutBox({
-            applicationName: 'Logs Indexer v0.2.6'
+            applicationName: 'Logs Indexer v0.2.7'
         });
         $scope.showAboutBox = aboutBox.modal.show;
     });
