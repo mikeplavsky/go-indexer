2c11ed8dcabcc68c31ae0b0372bf276b9cc3c0df
mikeplavsky <mike.plavsky@quest.com>
2015-05-07 16:53:33 +0000

    checking for _id

--- go-convert/converter/converter.go
+++ go-convert/converter/converter.go
@@ -59,6 +59,10 @@ func worker(
 			idx := [2]string{}
 			lineID := id + strconv.Itoa(e.num)
 
+			if obj["_id"] != nil {
+				lineID = obj["_id"].(string)
+			}
+
 			idx[0] = fmt.Sprintf(
 				`{"index": {"_type": "log","_id":"%v"}}`,
 				lineID)
