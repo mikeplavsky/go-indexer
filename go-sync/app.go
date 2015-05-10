4f47d6d806ec5332a24972c6065b78ed46b352b3
mikeplavsky <mike.plavsky@quest.com>
2015-05-11 02:00:16 +0300

    message parser

--- go-sync/app.go
+++ go-sync/app.go
@@ -1,19 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"go-indexer/go-sync/sqs"
 	"log"
 )
 
+type S3 struct {
+	Bucket map[string]interface{} `json:"bucket"`
+	Object map[string]interface{} `json:"object"`
+}
+
+type Record struct {
+	S3 S3 `json:"s3"`
+}
+
+type Message struct {
+	Records []Record
+}
+
+type Event struct {
+	Message string
+}
+
 func main() {
 
 	sqs := sqs.Sqs{}
-	msg, err := sqs.GetMessage()
+	res, err := sqs.GetMessage()
 
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	log.Println(msg)
+	raw := res.Body
+
+	var msg Event
+	err = json.Unmarshal([]byte(raw), &msg)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var rs Message
+	err = json.Unmarshal([]byte(msg.Message), &rs)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(rs)
 
 }
