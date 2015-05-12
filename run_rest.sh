ec1bd623429a8045819c47bb24af7f1d81e67362
mikeplavsky <mike.plavsky@quest.com>
2015-05-12 12:41:59 +0000

    fixed permissions and typo

--- run_rest.sh
+++ run_rest.sh
@@ -2,4 +2,4 @@ docker run -d \
 -e ES_QUEUE=$(curl http://169.254.169.254/latest/meta-data/instance-id) \
 --name rest-srv \
 --net=host \
---restart=alway go_indexer rest-srv
+--restart=always go_indexer rest-srv
