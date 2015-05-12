4dde030c347b9dd10a57ad7a6265f94bfdfe342f
mikeplavsky <mike.plavsky@quest.com>
2015-05-12 13:36:22 +0000

    do not need to run sync - all changes are in the queue already

--- run_stack.sh
+++ run_stack.sh
@@ -9,15 +9,11 @@ ES_QUEUE=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 ./start_stack.sh
 ./run_esspeed.sh
 
-while ! curl -XPUT localhost:8080/s3data --data-binary @s3-2-es/index.json
-do
-	sleep 1
-done
-
-./run_sync.sh
 ./run_s3.sh
 ./run_rest.sh
 ./run_kibana.sh
 ./run_nginx.sh
 
 ./start_loaders.sh
+
+./run_sync.sh
