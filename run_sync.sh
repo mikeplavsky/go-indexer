165479dbf0ac218724903cd4600ec319361b6f0c
mikeplavsky <mike.plavsky@quest.com>
2015-05-12 13:11:14 +0000

    right sync run

--- run_sync.sh
+++ run_sync.sh
@@ -2,10 +2,8 @@ docker rm -f go-sync
 
 ES_QUEUE=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 
-docker run \
--e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
--e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
--e ES_QUEUE=$ES_QUEUE"_sns"
+docker run -d \
+-e ES_QUEUE=$ES_QUEUE"_sns" \
 --name go-sync \
 --net=host \
 --restart=always go_indexer go-sync
