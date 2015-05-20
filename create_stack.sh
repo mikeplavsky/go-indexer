ebba60ce0efced468df37955b11111bc4a3dd3e5
mikeplavsky <mike.plavsky@quest.com>
2015-05-20 17:06:01 +0000

    fielddata heap size

--- create_stack.sh
+++ create_stack.sh
@@ -11,6 +11,7 @@ let port=i+8080
 docker create \
 -m 15g \
 -v /data/data$i:/data \
+-v $(pwd)/elasticsearch.yml:/elasticsearch/config/elasticsearch.yml:ro \
 -p $port:9200 \
 -e ES_CLUSTER_NAME=es \
 -e ES_HEAP_SIZE=3g \
