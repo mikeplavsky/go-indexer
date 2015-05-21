a95256d65a9cebe28dbd6b13bba26ddb7833442c
mikeplavsky <mike.plavsky@quest.com>
2015-05-21 12:56:46 +0000

    added go-indexer build to run stack

--- run_stack.sh
+++ run_stack.sh
@@ -1,6 +1,7 @@
 ES_STACK_NUM=$(cat /proc/cpuinfo | grep processor | wc -l)
 ES_QUEUE=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 
+./prep_indexer.sh
 ./route53.sh
 
 ./create_queues.sh
