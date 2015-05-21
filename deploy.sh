8b552a064c5b25cbd22299431c77f0ff8389d3d2
mikeplavsky <mike.plavsky@quest.com>
2015-05-21 15:30:58 +0000

    CF tweaks

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 git archive -o go-indexer.zip HEAD
+
 aws s3 cp go-indexer.zip s3://logmanagement/
+aws s3 cp CloudFormationD.json s3://logmanagement/
+aws s3 cp CloudFormationS.json s3://logmanagement/
+
 rm go-indexer.zip
