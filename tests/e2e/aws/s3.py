56a9f466d59495b3352583242d379972e9d55c8f
mikeplavksy <mike.plavsky@quest.com>
2015-06-08 20:00:47 +0000

    started working with s3

--- tests/e2e/aws/s3.py
+++ tests/e2e/aws/s3.py
@@ -1,11 +1,15 @@
 from waferslim.converters import converter_for
+import boto
 
 class S3Buckets(object):
     
     def query(self):
 
+        s3 = boto.connect_s3()
+
+        bs = s3.get_all_buckets()
+        res = [[['Name',x.name]] for x in bs]
+
         converter = converter_for(list)
-        return converter.to_string(
-               [[['Name','One'],['Last','Two']],    
-                [['Name','Two'],['Last','Three']]])    
+        return converter.to_string(res)
 
