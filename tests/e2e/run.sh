9aa9745f835fbf176c9a7e292521a0270088eb29
mikeplavsky <mike.plavsky@quest.com>
2015-06-30 20:17:04 +0000

    renamed fixtures for better understanding of the source

--- tests/e2e/run.sh
+++ tests/e2e/run.sh
@@ -3,7 +3,7 @@ docker rm -f fit
 docker run -d \
 --net=host \
 -v $(pwd)/FitNesseRoot/LogMan:/FitNesseRoot/LogMan \
--v $(pwd)/fixtures:/fixtures \
+-v $(pwd)/restslim:/fixtures \
 -v $(pwd)/data:/data \
 -v $(pwd)/aws:/aws \
 --name fit mikeplavsky/docker-waferslim
